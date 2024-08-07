7db43d7970052f7a7f22a2b4f2c985da5d980583
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-08-07 15:50:04 +0200

    Create monuments router

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,17 @@
 import chalk from "chalk";
 import express from "express";
+import { monumentsRouter } from "../router/monumentsRouter.js";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log(chalk.magentaBright("Listening on port 3000"));
-});
+const startApp = () => {
+  const port = process.env.PORT ?? 3100;
+
+  app.listen(port, () => {
+    console.log(chalk.magentaBright(`Listening on port ${port}`));
+  });
+};
+
+startApp();
+
+app.use("/monuments", monumentsRouter);
