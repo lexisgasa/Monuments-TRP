fc139383823a56dcb5dd076b2a52674eff34506c
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-08-07 11:49:15 +0200

    Create environment variable and startApp function in app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,8 +4,14 @@ import { monumentsRouter } from "../router/monumentsRouter.js";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log(chalk.magentaBright("Listening on port 3000"));
-});
+const startApp = () => {
+  const port = process.env.PORT ?? 3100;
+
+  app.listen(port, () => {
+    console.log(chalk.magentaBright(`Listening on port ${port}`));
+  });
+};
+
+startApp();
 
 app.use("/monuments", monumentsRouter);
