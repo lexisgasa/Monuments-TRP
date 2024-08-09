7e48e82c459ab48984e827d2e0529056d125a9ef
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-08-09 16:21:17 +0200

    Apply dependencies inversion principle

--- src/router/monumentsRouter.ts
+++ src/router/monumentsRouter.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { MonumentsController } from "../monuments/controller/MonumentsController/MonumentsController.js";
+import { monuments } from "../monuments/data.js";
 
 export const monumentsRouter = express.Router();
 
-const monumentsController = new MonumentsController();
+const monumentsController = new MonumentsController(monuments);
 
 monumentsRouter.get("/", monumentsController.getMonuments);
